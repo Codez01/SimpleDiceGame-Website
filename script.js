670386003c268b4ff3b32d55684a04c6168103ee
Codez01 <80861363+Codez01@users.noreply.github.com>
2021-08-10 14:06:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var player1  = parseInt( Math.random() * (7 - 1) + 1);
 var player2  = parseInt( Math.random() * (7 - 1) + 1);
 document.querySelector(".img1").src = "images/dice"+player1+".png";
 document.querySelector(".img2").src = "images/dice"+player2+".png"; 
+
 if(player1>player2){
 document.querySelector("h1").innerText = "Player1 won!"
 
@@ -16,12 +17,17 @@ document.querySelector("h1").innerText = "Draw!"
 
 }
 
-else{
+if(player2> player1){
   document.querySelector("h1").innerText = "Player2 won!"
  
 
 }
+else{
+
+
+}
 } else {
+
 
 
 }
